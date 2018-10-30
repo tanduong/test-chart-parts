82158763832629904848e12a7dd4d48f29a5a1d8
Tan Duong <tan.duong@kamereo.vn>
2018-10-31 02:24:01 +0700

    Chore: Add chart

---
+++ src/components/AnotherBarChart.js
@@ -0,0 +1,93 @@
+import * as React from 'react';
+import {Renderer} from '@chart-parts/react-svg-renderer';
+import {scene, rect, area, axis} from '@chart-parts/builder';
+import {Dimension, AxisOrientation} from '@chart-parts/interfaces';
+import {linear, time, band} from '@chart-parts/scales';
+import {Orchestrator} from '@chart-parts/orchestrator';
+
+const renderer = new Renderer();
+const pipeline = new Orchestrator(renderer);
+
+const data = [];
+for (let month = 1; month <= 12; ++month) {
+  for (let day = 1; day <= 29; ++day) {
+    const date = new Date(2017, month, day);
+    const amount = Math.random() * 100;
+    data.push({date, amount});
+  }
+}
+
+/**
+ * Adapted from https://vega.github.io/vega/examples/bar-chart/
+ */
+export default class AnotherBarChart extends React.Component {
+  chart = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {hoverRowIndex: undefined};
+    const isHovered = index => this.state.hoverRowIndex === index;
+
+    this.chart = scene(
+      n =>
+        n
+          .scale(
+            linear('y')
+              .domain('data.amount')
+              .range(Dimension.Height)
+              .nice(),
+            time('x')
+              .domain('data.date')
+              .range(Dimension.Width)
+              .nice(),
+            band('xband', 'xbandwidth')
+              .domain('data.date')
+              .range(Dimension.Width)
+          )
+          .axes(
+            axis('y', AxisOrientation.Left),
+            axis('x', AxisOrientation.Bottom)
+              .tickCount(4)
+              .labelFormat('%b %m %d')
+          )
+          .mark(
+            area('dataline')
+              .table('data')
+              .encode({
+                x: ({d, xband}) => xband(d.date),
+                y: ({d, y}) => y(d.amount),
+                y2: ({y}) => y(0),
+                stroke: () => 'black',
+                strokeWidth: () => 0.5,
+                fill: () => 'green',
+              }),
+            rect('highlight')
+              .table('data')
+              .encode({
+                x: ({d, xband}) => xband(d.date),
+                y: ({d, y}) => y(d.amount),
+                y2: ({y}) => y(0),
+                width: ({xbandwidth}) => xbandwidth(),
+                fill: ({index}) => (isHovered(index) ? 'firebrick' : 'transparent'),
+              })
+              .handle({
+                onMouseEnter: ({index}) => {
+                  if (this.state.hoverRowIndex !== index) {
+                    this.setState({hoverRowIndex: index});
+                  }
+                },
+                onMouseLeave: ({index}) => {
+                  if (this.state.hoverRowIndex === index) {
+                    this.setState({hoverRowIndex: undefined});
+                  }
+                },
+              })
+          ),
+      {width: 400, height: 200}
+    ).build();
+  }
+
+  render() {
+    return pipeline.renderScene(this.chart, {width: 400, height: 200, padding: 30}, {data});
+  }
+}
---
+++ src/components/BarChart.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import {Renderer} from '@chart-parts/react-svg-renderer';
+import {scene, rect, axis} from '@chart-parts/builder';
+import {Dimension, AxisOrientation} from '@chart-parts/interfaces';
+import {linear, band} from '@chart-parts/scales';
+import {Orchestrator} from '@chart-parts/orchestrator';
+
+const renderer = new Renderer();
+const pipeline = new Orchestrator(renderer);
+
+const data = [
+  {category: 'A', amount: 28},
+  {category: 'B', amount: 55},
+  {category: 'C', amount: 43},
+  {category: 'D', amount: 91},
+  {category: 'E', amount: 81},
+  {category: 'F', amount: 53},
+  {category: 'G', amount: 19},
+  {category: 'H', amount: 87},
+];
+
+export default class BarChart extends React.Component {
+  chart = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {hoverRowIndex: undefined};
+    const isHovered = index => this.state.hoverRowIndex === index;
+
+    this.chart = scene(
+      n =>
+        n
+          .scale(
+            linear('y')
+              .domain('data.amount')
+              .range(Dimension.Height)
+              .nice(),
+            band('x', 'xband')
+              .domain('data.category')
+              .range(Dimension.Width)
+              .padding(0.05)
+          )
+          .axes(
+            axis('x', AxisOrientation.Bottom),
+            axis('y', AxisOrientation.Left),
+            axis('x', AxisOrientation.Top),
+            axis('y', AxisOrientation.Right)
+          )
+          .mark(
+            rect()
+              .table('data')
+              .encode({
+                x: ({d, x}) => x(d.category),
+                y: ({d, y}) => y(d.amount),
+                y2: ({y}) => y(0),
+                width: ({xband}) => xband(),
+                fill: ({index}) => (isHovered(index) ? 'firebrick' : 'steelblue'),
+              })
+              .handle({
+                onMouseEnter: ({index}) => {
+                  if (this.state.hoverRowIndex !== index) {
+                    this.setState({hoverRowIndex: index});
+                  }
+                },
+                onMouseLeave: ({index}) => {
+                  if (this.state.hoverRowIndex === index) {
+                    this.setState({hoverRowIndex: undefined});
+                  }
+                },
+              })
+          ),
+      {width: 400, height: 200}
+    ).build();
+  }
+
+  render() {
+    return pipeline.renderScene(this.chart, {width: 400, height: 200, padding: 30}, {data});
+  }
+}
